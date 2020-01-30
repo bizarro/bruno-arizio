3d323bbef4003bad55814729f3f6989b665bbc9c
Luis Henrique Bizarro <hello@lhbzr.com>
2020-01-30 18:01:17 -0300

    Remove unused configuration file.

--- prismic-configuration.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  accessToken: 'MC5YTGZpTkJRQUFGWmk1eEp1.77-9UzEf77-977-977-977-977-9We-_ve-_ve-_ve-_ve-_vSo4aO-_ve-_ve-_vQgmSO-_vXLvv71o77-977-9XO-_vQ',
-
-  clientId: 'XLfiNBQAAFZi5xJt',
-  clientSecret: '5be7d81c9c1b8ecd0da6d71ba1edf9f5',
-
-  endpoint: 'https://bruno-arizio.prismic.io/api/v2'
-}
